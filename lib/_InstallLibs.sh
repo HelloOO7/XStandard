73787cdbb56c7fd5721ac3427fba48151c0ee233
HelloOO7 <cendarehor@gmail.com>
2026-04-09 22:01:34 +0200

    Add Linux version of _InstalLibs.sh (for CI).

---
+++ lib/_InstallLibs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mvn install:install-file -Dfile=frankenjfx-1.0.jar -DgroupId=frankenjfx -DartifactId=frankenjfx -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=frankenjfx-natives-win32-1.0.jar -DgroupId=frankenjfx -DartifactId=natives-win -Dversion=1.0 -Dpackaging=jar
